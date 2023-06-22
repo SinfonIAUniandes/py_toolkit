70c0afd253b7780a8bcd6e65e8bd7b163176502e
SnowArtz <ds.ortiz@uniandes.edu.co>
2023-06-22 16:28:24 -0500

    trasnformation

--- src/py_toolkit.py
+++ src/py_toolkit.py
@@ -6,6 +6,7 @@ import time
 import rospy
 import rospkg
 import argparse
+import almath
 import sys
 from robot_toolkit_msgs.srv import tablet_service_srv, go_to_posture_srv, go_to_posture_srvResponse, tablet_service_srvResponse, go_to_posture_srvRequest, set_output_volume_srv, set_output_volume_srvResponse, set_security_distance_srv, set_security_distance_srvResponse, get_input_srv, set_speechrecognition_srv, point_at_srv, point_at_srvResponse, set_open_close_hand_srv, set_open_close_hand_srvResponse, point_at_srvRequest
 from robot_toolkit_msgs.msg import text_to_speech_status_msg, speech_recognition_status_msg 
@@ -359,7 +360,8 @@ class PyToolkit:
 
 
     def on_color_blob_detected(self, value):
-        print(value)
+        #print(value)
+        print(almath.position3DFromPosition6D(value[0]))
 
 if __name__ == '__main__':
     consoleFormatter=ConsoleFormatter.ConsoleFormatter()
