a119411448fb466cf7fecd3abd4d1c142d3d30c3
SnowArtz <ds.ortiz@uniandes.edu.co>
2023-06-28 18:15:53 -0500

    arms F

--- src/py_toolkit.py
+++ src/py_toolkit.py
@@ -190,6 +190,7 @@ class PyToolkit:
         self.ALMotion.setOrthogonalSecurityDistance(req.distance)
         self.ALMotion.setTangentialSecurityDistance(req.distance)
         self.ALMotion.setCollisionProtectionEnabled("Arms", False)
+        self.ALMotion.setExternalCollisionProtectionEnabled("Arms", False)
         print(consoleFormatter.format('Security distance was set to '+str(req.distance)+' m', 'OKGREEN'))
         return set_security_distance_srvResponse("OK")
     
